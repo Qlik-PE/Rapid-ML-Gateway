79eee8ba5777c746da20152c58fea90d0c6f0f92
Parkman328 <jwpark.3@gmail.com>
2020-09-23 17:28:57 -0700

    Changes to Services

---
+++ cognitive_services/cognitive_services.py
@@ -0,0 +1,21 @@
+import json
+import logging
+import logging.config
+import os, sys, inspect, time
+PARENT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(os.path.join(PARENT_DIR, 'helper_functions'))
+
+def translate(url):
+    return 0
+
+def language_detection(url):
+    return 0
+
+def key_phrase_extraction(url):
+    return 0
+
+def sentiment_analysis(url):
+    return 0
+
+def annmaly_detection(url):
+    return 0
