9b02c8a63cb83fb779d00ad08a0e19c01d2146b8
Parkman328 <jwpark.3@gmail.com>
2021-01-15 15:02:22 -0800

    bug hunting jrp

--- helper_functions/peloton.py
+++ helper_functions/peloton.py
@@ -7,7 +7,6 @@ def get_all_instructors(url):
     result = json.loads(s.text)
     return_val = result["data"]
     return return_val 
-   
 
 def get_instructor(url, instructor_id):
     s = requests.get(url+"/"+instructor_id)
@@ -29,13 +28,11 @@ def get_all_sessions(user_name, password):
     S_userid = result['user_id']
     D_userdata = result['user_data']
     L_workout = result['user_data']['workout_counts']
-
     return s, result, D_userdata, L_workout, S_userid
 
 
 def get_all_workouts(s, url, user_id, option):
     url = url + user_id + '/workouts' + option
-    #print('Peleton url {}' .format(url))
     r = s.get(url)
     result = r.json()
     return result
@@ -43,9 +40,7 @@ def get_all_workouts(s, url, user_id, option):
 
 def get_all_details(s, url, workout_id, option):
     url = url + workout_id + option
-    #print(url)
-    r = s.get(url)
-    result = r.json()
+    result = s.get(url)
     return result
 
 
