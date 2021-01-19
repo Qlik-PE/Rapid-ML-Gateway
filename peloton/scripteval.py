7cea5cb9e9eafe4b66c801bbfd3f8798072c396d
Parkman328 <jwpark.3@gmail.com>
2021-01-19 15:46:17 -0800

    changes to temp

--- peloton/scripteval.py
+++ peloton/scripteval.py
@@ -383,18 +383,14 @@ class ScriptEval:
                 logging.debug('UserData type {} and UserData {}' .format(type(UserData), UserData))
                 if (len(remlist)) > 1:
                     temp = self.remove_columns_dict(remlist, UserData)
-                    logging.debug('Remoed UserData type {} and UserData {}' .format(type(UserData), UserData))
+                    logging.debug('Removed UserData type {} and UserData {}' .format(type(UserData), UserData))
                 else:
                     temp = UserData
                     logging.debug('No Var UserData type {} and UserData {}' .format(type(UserData), UserData))
-                if(temp in locals()):
-                    logging.debug('Temp type {} and Temp {}' .format(type(temp), temp))
-                    converted = qlist.convert_dicts_list(temp)
-                    result.append(converted[1])
                 logging.debug('Temp type {} and Temp {}' .format(type(temp), temp))
                 converted = qlist.convert_dicts_list(temp)
                 result.append(converted[1])
-           
+                
             table.name= User +'- Peloton Output Data'
             for i in converted[0]:
               FieldName = i
