767ff9a057d1bea57bae472b26e46cf41ab8ec2f
Parkman328 <jwpark.3@gmail.com>
2021-01-23 00:50:35 -0800

    type get tickers

--- pyfinance/scripteval.py
+++ pyfinance/scripteval.py
@@ -272,7 +272,7 @@ class ScriptEval:
             logging.debug("get tickers - tickers: {} Arguments {} start_date : {} end_date :{} attrib :{} " .format(tickers, Arguments, start_date, end_date, attrib))
             result = self.get_tickers(tickers, start_date, end_date, attrib)
             converted = qlist.convert_df_list(result)
-            table.name= ' '.join([str(elem) for elem in ticker_list]) + '- Data'
+            table.name= ' '.join([str(elem) for elem in tickers]) + '- Data'
             logging.debug("column  {}" .format(converted[0]))
             for i in converted[0]:
                 FieldName = i
