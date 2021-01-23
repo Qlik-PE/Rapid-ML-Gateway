b9fc4538855e8aa6467d6c165cc3953cc2277784
Parkman328 <jwpark.3@gmail.com>
2021-01-23 02:32:27 -0800

    logging

--- pyfinance/scripteval.py
+++ pyfinance/scripteval.py
@@ -328,6 +328,7 @@ class ScriptEval:
             attrib = Arguments[2]
             logging.debug("get_Cov_Matrix - tickers: {} Arguments {} start_date : {} end_date :{} attrib :{} " .format(tickers, Arguments, start_date, end_date, attrib))
             result = self.get_Cov_Matrix(tickers, start_date, end_date, attrib)
+            logging.debug("result - type: {} data: {} " .format(type(result), result))
             converted = qlist.convert_df_list(result)
             table.name= ' '.join([str(elem) for elem in tickers]) + '-' + attrib + '- Cov Matrix'
             logging.debug("column  {}" .format(converted[0]))
