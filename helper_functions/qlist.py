7b0a760a1380b705112c101d0c4fb91976e7691a
Parkman328 <jwpark.3@gmail.com>
2021-01-27 14:25:36 -0800

    rounding issues

--- helper_functions/qlist.py
+++ helper_functions/qlist.py
@@ -51,7 +51,7 @@ def convert_df_list(input_df):
     for x in temp_dict['data']:
         x.insert(0, np.datetime_as_string(input_df.index.values[i], unit='D'))
         i +=1
-        temp = [str(y) for y in x]
+        temp = [str(rounnd(y,6)) for y in x]
         print('Temp {}', format(temp))
         values.append(temp)
     return columns, values
